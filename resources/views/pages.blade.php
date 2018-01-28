ce91f6bcb2834b21d294d7d9511deeaf4998443c
Cameron McHenry <camchenry@users.noreply.github.com>
2018-01-28 02:10:44 +0000

    Improve search results layout

--- resources/views/pages.blade.php
+++ resources/views/pages.blade.php
@@ -16,41 +16,54 @@
              api-key="{{ env('ALGOLIA_SEARCH_KEY') }}"
              index-name="live_man_pages"
              >
-             <ais-search-box>
-                 <div class="input-group">
-                     <ais-input
-                      placeholder="Search..."
-                      :class-names="{'ais-input': 'mt-2 mb-3 form-control form-control-lg'}"
-                      />
+             <div class="row">
+                 <div class="col">
+                     <ais-search-box>
+                         <div class="input-group">
+                             <ais-input
+                              placeholder="Search..."
+                              :class-names="{'ais-input': 'mt-2 mb-3 form-control form-control-lg'}"
+                              />
 
-                     <span class="input-group-btn">
-                         <ais-clear :class-names="{'ais-clear': 'btn btn-default'}">
-                             <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
-                             </ais-clear>
-                             <button class="btn btn-default" type="submit">
-                                 <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
-                             </button>
-                     </span>
-                 </div><!-- /input-group -->
+                             <span class="input-group-btn">
+                                 <ais-clear :class-names="{'ais-clear': 'btn btn-default'}">
+                                     <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
+                                     </ais-clear>
+                                     <button class="btn btn-default" type="submit">
+                                         <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+                                     </button>
+                             </span>
+                         </div><!-- /input-group -->
 
-            </ais-search-box>
+                    </ais-search-box>
+                 </div>
+             </div>
+             <div class="row">
+                 <div class="col-md-3">
+                     <h4>Filters</h4>
+                 </div>
+                 <div class="col-md-9">
                 <ais-results>
                     <template slot-scope="{ result }">
-                        <div>
-                        <h3>
-                            <a :href="'{{ URL::to('/pages') }}' + '/' + result.name">
-                                <ais-highlight :result="result" attribute-name="name"></ais-highlight>
-                            </a>
-                        </h3>
-                        <p class="lead">
-                            <ais-highlight :result="result" attribute-name="short_description"></ais-highlight>
-                        </p>
-                        <p class="text-muted">
-                            <ais-highlight :result="result" attribute-name="description"></ais-highlight>
-                        </p>
+                        <div class="card mb-2">
+                            <div class="card-body">
+                                <h3>
+                                    <a :href="'{{ URL::to('/pages') }}' + '/' + result.name">
+                                        <ais-highlight :result="result" attribute-name="name"></ais-highlight>
+                                    </a>
+                                </h3>
+                                <p class="lead">
+                                <ais-highlight :result="result" attribute-name="short_description"></ais-highlight>
+                                </p>
+                                <p class="text-muted">
+                                <ais-highlight :result="result" attribute-name="description"></ais-highlight>
+                                </p>
+                            </div>
                         </div>
                     </template>
                 </ais-results>
+                 </div>
+             </div>
             </ais-index>
         </div>
     </div>
