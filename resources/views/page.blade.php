f82b82c171f01e594ff4d3f17e0a340e7d13f390
Cameron McHenry <camchenry@users.noreply.github.com>
2018-02-16 14:12:28 +0000

    Remove extra semicolon

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -2,7 +2,7 @@
 
 @section('title', $page->name)
 
-@section('meta.description', $page->short_description);
+@section('meta.description', $page->short_description)
 
 @section('scripts')
     <script src="https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/1.7.1/clipboard.min.js" async></script>
