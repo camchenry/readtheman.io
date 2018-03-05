689886ad0324631a218b44b4586d1248c3ee288f
Cameron McHenry <camchenry@users.noreply.github.com>
2018-03-05 00:35:10 +0000

    Add fast mode for coreutils

--- app/Console/Commands/GetCoreutilsManPages.php
+++ app/Console/Commands/GetCoreutilsManPages.php
@@ -42,95 +42,116 @@ class GetCoreutilsManPages extends Command
      */
     public function handle()
     {
-        echo "Installing prerequisite packages.\n";
-        $packages = "autoconf automake autopoint bison gettext gperf git gzip perl rsync tar texinfo";
-        echo "Installing: \n\t" . implode("\n\t", explode(' ', $packages)) . "\n";
-        // Prerequisities
-        $process = new Process("sudo apt-get -y install {$packages}");
-        $process->run();
-
-        $directory = storage_path() . '/man_pages/coreutils';
-        $github_url = 'https://github.com/coreutils/coreutils';
-        echo "Fetching coreutils git repository ({$github_url})\n";
-        if (!file_exists($directory)) {
-            $repository = \Gitonomy\Git\Admin::cloneTo($directory, $github_url, false);
-        }
-        else {
-            $process = new Process("sudo su && cd $directory && git pull");
-            $process->run();
-
-            if (!$process->isSuccessful())
-            {
-                exit($process->getErrorOutput());
+        $page_queue = [];
+        if ($this->option('fast')) {
+            $pages = \App\Page::where('source', '=', 'Coreutils')->get();
+            foreach($pages as $page) {
+                array_push($page_queue, [
+                    'name' => trim($page->name),
+                    'section' => trim($page->section),
+                    'raw_html' => trim($page->raw_html),
+                ]);
             }
         }
+        else {
+            echo "Installing prerequisite packages.\n";
+            $packages = "autoconf automake autopoint bison gettext gperf git gzip perl rsync tar texinfo";
+            echo "Installing: \n\t" . implode("\n\t", explode(' ', $packages)) . "\n";
+            // Prerequisities
+            $process = new Process("sudo apt-get -y install {$packages}");
+            $process->run();
 
-        $section = 1;
+            $directory = storage_path() . '/man_pages/coreutils';
+            $github_url = 'https://github.com/coreutils/coreutils';
+            echo "Fetching coreutils git repository ({$github_url})\n";
+            if (!file_exists($directory)) {
+                $repository = \Gitonomy\Git\Admin::cloneTo($directory, $github_url, false);
+            }
+            else {
+                $process = new Process("sudo su && cd $directory && git pull");
+                $process->run();
 
-        $commands = [
-            [
-                'command' => "sudo rm -rf {$directory}/autom4te.cache",
-            ],
-        ];
+                if (!$process->isSuccessful())
+                {
+                    exit($process->getErrorOutput());
+                }
+            }
 
-        if (!$this->option('fast')) {
+            $section = 1;
+
+            $commands = [
+                [
+                    'command' => "sudo rm -rf {$directory}/autom4te.cache",
+                ],
+            ];
+
             array_push($commands,
                 [
                     'message' => "Bootstrapping coreutils.\n",
                     'command' => "cd {$directory} && ./bootstrap --skip-po",
                     'timeout' => 900,
+                ],
+                [
+                    'message' => "Configuring coreutils.\n",
+                    'command' => "cd {$directory} && ./configure -C",
+                    'timeout' => 900,
+                ],
+                [
+                    'message' => "Running automake.\n",
+                    'command' => "cd {$directory} && make",
+                    'timeout' => 900,
+                ],
+                [
+                    'message' => "Creating man directory structure.\n",
+                    'command' => "mkdir -p {$directory}/man/man{$section}",
+                ],
+                [
+                    'message' => "Moving man pages into directories.\n",
+                    'command' => "mv {$directory}/man/*.{$section} {$directory}/man/man{$section}",
                 ]
             );
-        }
 
-        array_push($commands,
-            [
-                'message' => "Configuring coreutils.\n",
-                'command' => "cd {$directory} && ./configure -C",
-                'timeout' => 900,
-            ],
-            [
-                'message' => "Running automake.\n",
-                'command' => "cd {$directory} && make",
-                'timeout' => 900,
-            ],
-            [
-                'message' => "Creating man directory structure.\n",
-                'command' => "mkdir -p {$directory}/man/man{$section}",
-            ],
-            [
-                'message' => "Moving man pages into directories.\n",
-                'command' => "mv {$directory}/man/*.{$section} {$directory}/man/man{$section}",
-            ]
-        );
-
-        foreach($commands as $command) {
-            if (isset($command['message'])) {
-                echo $command['message'];
+            foreach($commands as $command) {
+                if (isset($command['message'])) {
+                    echo $command['message'];
+                }
+                $process = new Process($command['command']);
+                $process->setTimeout($command['timeout'] ?? 60);
+                $process->run();
+                if (!$process->isSuccessful())
+                {
+                    exit($process->getErrorOutput());
+                }
             }
-            $process = new Process($command['command']);
-            $process->setTimeout($command['timeout'] ?? 60);
-            $process->run();
-            if (!$process->isSuccessful())
-            {
-                exit($process->getErrorOutput());
+
+            $finder = new Finder();
+            $finder->files()->in($directory . '/man')->name("/(.*)\.(\d)/");
+
+            echo "Searching for files in {$directory}/man\n";
+
+            foreach($finder as $file) {
+                $filename = $file->getFilename();
+                preg_match('/(.*)\.(\d)/', $filename, $matches);
+                $page_name = trim($matches[1]);
+                $section = trim($matches[2]);
+                $raw_html = ImportHelper::makeHtmlForManPage($page_name, $section, $directory . '/man');
+
+                array_push($page_queue, [
+                    'name' => trim($page_name),
+                    'section' => trim($section),
+                    'raw_html' => trim($raw_html),
+                ]);
             }
         }
 
-        $finder = new Finder();
-        $finder->files()->in($directory . '/man')->name("/(.*)\.(\d)/");
-
-        echo "Searching for files in {$directory}/man\n";
-        foreach($finder as $file) {
-            $filename = $file->getFilename();
-            preg_match('/(.*)\.(\d)/', $filename, $matches);
-            $page_name = trim($matches[1]);
-            $section = trim($matches[2]);
+        foreach($page_queue as $page) {
+            $page_name = $page['name'];
+            $section = $page['section'];
+            $raw_html = $page['raw_html'];
+            $html = $raw_html;
 
             echo "Section {$section}, {$page_name}\n";
 
-            $html = ImportHelper::makeHtmlForManPage($page_name, $section, $directory . '/man');
-            $raw_html = $html;
             $doc = ImportHelper::createSectionedDocument($html);
 
             /*
