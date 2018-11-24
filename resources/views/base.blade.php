0300d36894019eb11ce0c33d6fe533b7d467cc43
Cameron McHenry <camchenry@users.noreply.github.com>
2018-11-24 23:15:32 +0000

    Change navbar color to primary theme color

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -24,25 +24,14 @@
         <nav class="navbar-main navbar navbar-expand-md navbar-dark bg-dark">
             <div class="container">
                 <a class="navbar-brand" href="{{ URL::to('/') }}">ReadTheMan</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item {{ Request::is('/') ? 'active' : '' }}">
-                            <a class="nav-link" href="{{ URL::to('/') }}">
-                                Home @if(Request::is('/'))<span class="sr-only">(current)</span>@endif
-                            </a>
-                        </li>
-                        <li class="nav-item {{ Request::is('pages') ? 'active' : '' }}">
-                            <a class="nav-link" href="{{ URL::to('/pages') }}">
-                                Pages @if(Request::is('/'))<span class="sr-only">(current)</span>@endif
-                            </a>
-                        </li>
-                    </ul>
-                    @include('shared.minisearch')
-                </div>
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item {{ Request::is('pages') ? 'active' : '' }}">
+                        <a class="nav-link" href="{{ URL::to('/pages') }}">
+                            Pages @if(Request::is('/'))<span class="sr-only">(current)</span>@endif
+                        </a>
+                    </li>
+                </ul>
+                @include('shared.minisearch')
             </div>
         </nav>
         <div class="full-page-content mb-3">
